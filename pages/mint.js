05d7f3ff9d040fa3ba3e2ea96e1db4d4b6ecdc34
Hemant <hrsmart2150@gmail.com>
2022-11-17 23:00:21 +0530

    made static mint page for now

---
+++ pages/mint.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+function Remix() {
+    return (
+        <div className='bg-gray h-[100vh] w-full' >
+
+            <div className='flex flex-col justify-center items-center tracking-[0.08em]'>
+                <h1 className='text-red pt-12 text-6xl'>YOUR REMIX IS READY !</h1>
+                <h3 className='text-white pt-8 text-5xl'>Name</h3>
+            </div>
+
+            <div className='flex justify-center space-x-16 text-4xl pt-16 tracking-[0.08em]'>
+                <div className='text-white'>
+                    <p>REVENUE</p>
+                    <p className='text-xl'>SHARE TO ORIGINAL</p>
+                </div>
+                <div className='text-green flex items-center font-bold'>
+                    <div className='py-3 px-2 border-[1px] border-white'>0.5%</div>
+                    <div className='py-3 px-2 border-[1px] border-white'>10%</div>
+                    <div className='py-3 px-2 border-[1px] border-white'>20%</div>
+                    <div className='py-3 px-2 border-[1px] border-white'>30%</div>
+                </div>
+            </div>
+
+            <div className='flex text-white justify-center text-3xl pt-16 tracking-[0.08em]'>
+                <p>Artists</p>
+            </div>
+
+            <div className='flex justify-center items-center space-x-16 text-4xl pt-8 tracking-[0.08em]'>
+                <div className='text-white text-2xl'>
+                    <p>YOUR SHARES</p>
+                </div>
+                <div className='text-green flex items-center text-2xl font-bold'>
+                    <div className='py-[5px] px-2 border-[1px] border-white'>70%</div>
+                </div>
+                <div>
+                    <button className='bg-yellow font-semibold text-black text-2xl px-3 py-[5px] border-2 border-black '>MINT</button>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Remix
