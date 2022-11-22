9af3105ccec736ac8970f3f57b2b37cea08f7fd3
Dishant Sharma <dishu5570@gmail.com>
2022-11-22 11:15:37 +0530

    songs merge

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CustomButton from '../components/CustomButton'
 import SafariModal from '../packages/Modals/SelectModal';
+import cd from '/assets/Main/Images/CD.svg'
 import { useState } from 'react';
 import dynamic from 'next/dynamic';
 
@@ -41,6 +42,8 @@ const songnames = [
 const Home = () => {
   const [open, setOpen] = useState(false);
   const [blob, setBlob] = useState(null)
+  const [duration, setDuration] = useState('')
+  const [progress, setProgress] = useState('')
   const [items, setItems] = useState({
     vocal: [{ name: 'COMMERCIAL', path: '/Songs/Song-Vocals_(COMMERCIAL)_1.mp3' }, { name: 'COMMERCIAL_NOHATE', path: '/Songs/Song-Vocals_(COMMERCIAL_NOHATE)_1.mp3' }, { name: 'EXCLUSIVE', path: '/Songs/Song-Keys_(EXCLUSIVE)_1.mp3' }, { name: 'PERSONAL', path: '/Songs/Song-Vocals_(PERSONAL)_1.mp3' }, { name: 'PERSONAL_NOHATE', path: '/Songs/Song-Vocals_(PERSONAL_NOHATE)_1.mp3' }, { name: 'PUBLIC_DOMAIN', path: '/Songs/Song-Vocals_(PUBLIC_DOMAIN)_1.mp3' }],
     guitar: [{ name: 'COMMERCIAL', path: '/Songs/Song-Keys_(COMMERCIAL)_1.mp3' }, { name: 'COMMERCIAL_NOHATE2', path: '/Songs/Song-Keys_(COMMERCIAL_NOHATE)_2.mp3' }, { name: 'COMMERCIAL_NOHATE', path: '/Songs/Song-Keys_(COMMERCIAL_NOHATE).mp3' }, { name: 'EXCLUSIVE', path: '/Songs/Song-Keys_(EXCLUSIVE)_1.mp3' }, { name: 'PERSONAL', path: '/Songs/Song-Keys_(PERSONAL)_1.mp3' }, { name: 'PERSONAL_NOHATE', path: '/Songs/Song-Keys_(PERSONAL_NOHATE)_1.mp3' }, { name: 'PUBLIC_DOMAIN', path: '/Songs/Song-Keys_(PUBLIC_DOMAIN)_1.mp3' }],
@@ -67,7 +70,18 @@ const Home = () => {
         <CustomButton onClick={() => { setSelectedOption('bass'); setOpen(true) }} text={'Bass'} />
         <CustomButton onClick={() => { setSelectedOption('drum'); setOpen(true) }} text={'Drum'} />
       </div>
-      <div className='mt-[6rem] flex flex-col gap-[2rem] items-center w-[100vw]'>
+      <div className='w-[100vw] flex justify-center mt-[4rem]'>
+        <div className='min-w-[25rem] gap-[1rem] px-[1rem] h-[4rem] flex justify-center items-center border border-gray rounded-[50px]'>
+          <img className={progress && `animate-spin`} src={cd.src} />
+          <div className='h-[5px] w-[20rem] bg-gray'>
+            <div style={{ width: `${(progress / duration) * 100}%` }} className='h-[5px] bg-yellow border border-gray'></div>
+          </div>
+          <p className='text-[12px]'>
+            {progress !== '' ? `${progress}/${duration}` : 'No Remix'}
+          </p>
+        </div>
+      </div>
+      <div className='mt-[2rem] flex flex-col gap-[2rem] items-center w-[100vw]'>
         <div className='w-[40rem] flex gap-[2rem] flex-col items-end'>
           <div className='flex gap-[1rem] items-center'>
             <div className='font-[Citizen-OT-Medium] text-[26px] text-gray font-[700]'>Vocal</div>
@@ -90,7 +104,8 @@ const Home = () => {
       <div className='w-[100vw] flex justify-center mt-[2rem]'>
         <button className='font-[Citizen-OT-Medium] flex items-center justify-center h-[4rem] w-[10rem] bg-yellow border border-gray text-[26px] text-gray font-[700]'>next</button>
       </div>
-      {blob !== null && <ReactPlayer loop={true} url={blob} playing />}
+
+      {blob !== null && <ReactPlayer onDuration={e => setDuration(e.toFixed(2))} onProgress={e => setProgress(e.playedSeconds.toFixed(2))} loop={true} url={blob} playing />}
       <SafariModal values={values} setValues={setValues} selectedOption={selectedOption} items={items[selectedOption]} open={open} setOpen={setOpen} />
     </div>
   )
